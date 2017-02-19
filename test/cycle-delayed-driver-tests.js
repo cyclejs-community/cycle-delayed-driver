15c89ecc569db1f32df5b5c8fe5534183317e899
Guy Rozen <HolyEggplant@gmail.com>
2017-02-19 12:01:09 +0200

    Added more tests. Most importantly, that the inner driver works once created.

--- test/cycle-delayed-driver-tests.js
+++ test/cycle-delayed-driver-tests.js
@@ -1,9 +1,14 @@
-import { expect } from 'chai';
+import { default as chai, expect } from 'chai';
+import { default as spies } from 'chai-spies';
 import xs from 'xstream';
 import { makeDelayedDriver } from '../src/cycle-delayed-driver';
 
+chai.use(spies);
+
 let testArray = [];
 let driverCreated = false;
+
+let delayedDriver = null;
 
 const arrayPushDriver = function(sink$) {
   sink$.addListener({
@@ -22,8 +27,8 @@ const pushDriverOnSix = function(thing) {
   return null;
 }
 
-// Causes a stream to evaluate, which in case of streams bulit upon rejected promises 
-// avoids the error associated with not handling the rejection.
+// Causes a stream to evaluate. Defined mainly for streams bulit upon rejected promises
+// to avoid the error associated with not handling the rejection.
 const drain = function(stream$) {
   stream$.addListener({ next: () => null });
 }
@@ -32,11 +37,12 @@ describe('Cycle Delayed Driver', () => {
   beforeEach(() => {
     testArray = [];
     driverCreated = false;
+
+    delayedDriver = makeDelayedDriver(pushDriverOnSix);
   });
 
   it('creates the inner driver when the proper item is received', () => {
     let streamWithSix = xs.of(1, 2, 6);
-    let delayedDriver = makeDelayedDriver(pushDriverOnSix);
 
     delayedDriver(streamWithSix);
 
@@ -45,10 +51,27 @@ describe('Cycle Delayed Driver', () => {
 
   it('does not create the inner driver when the proper item is not received', () => {
     let streamWithoutSix = xs.of(1, 2, 'cat');
-    let delayedDriver = makeDelayedDriver(pushDriverOnSix);
 
     drain(delayedDriver(streamWithoutSix));
 
     expect(driverCreated).to.be.false;
+  });
+
+  it('passes the stream to the inner driver once it is created', () => {
+    let inputStream = xs.of(1, 2, 6, 'oh', 'yay', 'it', 'worked');
+
+    delayedDriver(inputStream);
+
+    expect(testArray).to.eql(['oh', 'yay', 'it', 'worked']);
+  });
+
+  it('stops trying to create the inner driver once it has been created', () => {
+    let creationSpy = chai.spy(pushDriverOnSix);
+    let delayedDriver = makeDelayedDriver(creationSpy);
+    let inputStream = xs.of(1, 2, 6, 'oh', 'yay', 'it', 'worked');
+
+    delayedDriver(inputStream);
+
+    expect(creationSpy).to.have.been.called.exactly(3);
   });
 });
